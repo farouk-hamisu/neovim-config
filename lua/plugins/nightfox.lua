fd4ca04be642a822057c04f213b6ccd04d9064be
farouk <hamisuf29@gmail.com>
2024-12-16 22:13:06 -0800

    configure nightfox colorscheme to enable transparent background

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -1,6 +1,31 @@
 return { 
 	"EdenEast/nightfox.nvim" , 
 	config = function()	
-		vim.cmd("colorscheme nightfox")
+    require("nightfox").setup({
+ options = {
+    transparent = true, -- Disable background transparency
+    terminal_colors = true, -- Use theme's colors for terminal
+    styles = {
+      comments = "italic", -- Italicize comments
+      keywords = "bold",   -- Bold keywords
+      functions = "italic,bold", -- Italicize and bold functions
+      variables = "NONE",
+    },
+  },
+  palettes = {
+    nightfox = {
+      bg0 = "#1e1e2e", -- Slightly darker background
+      sel0 = "#44475a", -- Selection color
+    },
+  },
+  groups = {
+    all = {
+      CursorLine = { bg = "#333842" }, -- Custom cursor line color
+      Visual = { bg = "#44475a" },     -- Custom visual selection
+    },
+  },
+})
+-- Set the colorscheme
+		vim.cmd("colorscheme carbonfox")
 	end
 }
